3db896aa11904033a9f93a87313697868840b1fe
jacobduncan00 <jacobduncan00@yahoo.com>
2020-11-19 19:51:29 -0500

    asd

---
+++ admin/admin.php
@@ -0,0 +1,129 @@
+<?php
+echo '<head>
+  <meta charset="utf-8">
+
+  <title>SU COVID-19 DB</title>
+  <meta name="description" content="">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="./adminstyles.css">
+</head>
+
+<body style="font-family: sans-serif;">
+	<div class="topnav">
+	  <a href="../homepage/homepage.html">Home</a>
+	  <a href="insert.html">Insert</a>
+	  <a href="../search/search.html">Search</a>
+	  <a href="../about/about.html">About</a>
+	  <div style="float:right;" class="topnav-right">
+	    <a class="active" href="admin.html">Admin</a>
+	    <a href="../homepage/homepage.html">Logout</a>
+	    <a href="../contact/contact.html">Contact</a>
+	  </div>
+	</div>
+</body>';
+
+ $connection=@mysqli_connect('localhost', 'jciurca1', 'jciurca1', 'CovidDB');
+ $query="SELECT Staff.ID, birthdate, name, email FROM Staff, University_personnel WHERE Staff.ID = University_personnel.ID;";
+ $r = mysqli_query($connection, $query);
+
+ // STAFF
+ echo "<h1 style='text-align: center;'>STAFF</h1>";
+ echo "<table style='margin-top:2px' id='staff' border='1'>
+        <thead>
+        <tr>
+                <th> ID </th>
+                <th> DOB </th>
+                <th> Name  </th>
+                <th> Email  </th>
+        </tr>
+        </thead>";
+
+while($row=mysqli_fetch_array($r)) {
+        echo "<tr>";
+        echo "<td>" . $row['ID'] . "</td>";
+        echo "<td>" . $row['birthdate'] . "</td>";
+        echo "<td>" . $row['name'] . "</td>";
+ 	echo "<td>" . $row['email'] . "</td>";
+        echo "</tr>";
+}
+echo "</table>";
+
+// STUDENT
+ $query="SELECT Student.ID, birthdate, name, email FROM Student, University_personnel WHERE Student.ID = University_personnel.ID;";
+ $r = mysqli_query($connection, $query);
+
+ echo "<h1 style='text-align: center;'>STUDENT</h1>";
+ echo "<table style='margin-top:2px' id='staff' border='1'>
+        <thead>
+        <tr>
+                <th> ID </th>
+                <th> DOB </th>
+                <th> Name  </th>
+                 <th> Email  </th>
+        </tr>
+        </thead>";
+
+while($row=mysqli_fetch_array($r)) {
+        echo "<tr>";
+        echo "<td>" . $row['ID'] . "</td>";
+        echo "<td>" . $row['birthdate'] . "</td>";
+        echo "<td>" . $row['name'] . "</td>";
+ 	echo "<td>" . $row['email'] . "</td>";
+        echo "</tr>";
+}
+echo "</table>";
+
+
+
+
+// FACULTY 
+ $query="SELECT Faculty.ID, birthdate, name, email FROM Faculty, University_personnel WHERE Faculty.ID = University_personnel.ID;";
+ $r = mysqli_query($connection, $query);
+
+ echo "<h1 style='text-align: center;'>FACULTY</h1>";
+ echo "<table style='margin-top:2px' id='staff' border='1'>
+        <thead>
+        <tr>
+                <th> ID </th>
+                <th> DOB </th>
+                <th> Name  </th>
+                 <th> Email  </th>
+        </tr>
+        </thead>";
+
+while($row=mysqli_fetch_array($r)) {
+        echo "<tr>";
+        echo "<td>" . $row['ID'] . "</td>";
+        echo "<td>" . $row['birthdate'] . "</td>";
+        echo "<td>" . $row['name'] . "</td>";
+ 	echo "<td>" . $row['email'] . "</td>";
+        echo "</tr>";
+}
+echo "</table>";
+
+
+
+// COVID_test 
+ $query="SELECT Test_date, Test_time from COVID_test;";
+ $r = mysqli_query($connection, $query);
+
+ echo "<h1 style='text-align: center;'>COVID TESTS</h1>";
+ echo "<table style='margin-top:2px' id='staff' border='1'>
+        <thead>
+        <tr>
+                <th> Date </th>
+                <th> Time </th>
+        </tr>
+        </thead>";
+
+while($row=mysqli_fetch_array($r)) {
+        echo "<tr>";
+        echo "<td>" . $row['Test_date'] . "</td>";
+        echo "<td>" . $row['Test_time'] . "</td>";
+        echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($connection);
+?>
--- login/login.php
+++ login/login.php
@@ -1,21 +1,26 @@
 <?php  
 
  require('db_connect.php');
-	if (isset($_POST['user_id']) and isset($_POST['user_pass'])){
-			
-		$username = $_POST['user_id'];
-		$password = $_POST['user_pass'];
+
+// Checks to see if values are set
+// This means if the value is declared and is different than NULL
+// We don't want to query if either of the pieces of the informationa are NULL
+
+if (isset($_POST['user_id']) and isset($_POST['user_pass'])){
 		
-		// Querying user_login table to see if we have an entry that matches the username and password entered 
-		$query = "SELECT * FROM `user_login` WHERE username='$username' and Password='$password'";
-		$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-		$count = mysqli_num_rows($result);
-		if ($count == 1){
-		 echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
-		 header("Location: ../admin/admin.html");
-		 exit;
-		}else{
-		 echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
-		}
+	$username = $_POST['user_id'];
+	$password = $_POST['user_pass']; // need to hash this eventually for security
+	
+	// Querying user_login table to see if we have an entry that matches the username and password entered 
+	$query = "SELECT * FROM `User_login` WHERE username='$username' and Password='$password'";
+	$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+	$count = mysqli_num_rows($result);
+
+	if ($count == 1){ // Means we got a tuple back, meaning our info was in the DB
+	 header("Location: ../admin/admin.php");
+	 exit;
+	}else{ // Else we didn't get anything back and therefore info was not in DB
+	 echo "<script type='text/javascript'>alert('Invalid Login Credentials'); window.location.href='./login.html';</script>";
 	}
+}
 ?>

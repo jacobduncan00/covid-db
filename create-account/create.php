dc914c0655beb09a6cbf534677c04cadcb33fc54
jacobduncan00 <jacobduncan00@yahoo.com>
2020-12-09 22:24:29 -0500

    fixed login / account creation issues

--- create-account/create.php
+++ create-account/create.php
@@ -12,15 +12,19 @@ if (isset($_POST['user_id']) and isset($_POST['user_pass'])){
 	$username = $_POST['user_id'];
 	$password = $_POST['user_pass']; // need to hash this eventually for security
 
-	if ($username == "" | $password == "") {
-	 echo "<script type='text/javascript'>alert('Could not create account! Empty fields'); window.location.href='../login/login.html';</script>";
-	}
-	
-	$query = "INSERT INTO User_login (username, password) VALUES ('%s', '%s')";
-	if(mysqli_query($connection, sprintf($query, $username, $password)) == TRUE) {
-		 echo "<script type='text/javascript'>alert('Account created!'); window.location.href='../login/login.html';</script>";
+
+
+	if (($username == undefined || $username == NULL) && ($password == undefined || $password == NULL)) {
+	 echo "<script type='text/javascript'>alert('Could not create account! Empty fields'); window.location.href='../create-account/createpage.php';</script>";
 	} else {
-	 echo "<script type='text/javascript'>alert('Could not create account!'); window.location.href='../login/login.html';</script>";
+	
+		$query = "INSERT INTO User_login (username, password) VALUES ('%s', '%s')";
+		if(mysqli_query($connection, sprintf($query, $username, $password)) == TRUE) {
+			 echo "<script type='text/javascript'>alert('Account created!'); window.location.href='../login/login.html';</script>";
+		} else {
+		 echo "<script type='text/javascript'>alert('Could not create account!'); window.location.href='../login/login.html';</script>";
+		}
+
 	}
 }
 ?>
